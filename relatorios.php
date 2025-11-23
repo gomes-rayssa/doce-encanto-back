ea72cced20af6f9e00eee2b4b48bd8266eedda3b
gomes-rayssa <gomes.rayssa@outlook.com.br>
2025-11-23 01:17:26 -0300

    .

--- relatorios.php
+++ relatorios.php
@@ -97,7 +97,6 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Relatórios e Estatísticas</title>
     <link rel="stylesheet" href="admin.css">
-    <link rel="stylesheet" href="accessibility.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
